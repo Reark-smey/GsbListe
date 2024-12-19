f5443ab24515e21e415d75ff84c6914bcedcf36c
Reark-smey <rearksmey.by@Lmdsio.fr>
2024-12-19 17:00:38 +0100

    gsb

---
+++ src/app/models/visiteur.ts
@@ -0,0 +1,26 @@
+export class Visiteur {
+  public id_visiteur: number = 0;
+  public nom_visiteur: string = '';
+  public prenom_visiteur: string = '';
+  public type_visiteur: string = '';
+  public login: string = '';
+  public password: string = '';
+
+  constructor(json?:any) {
+    if(json) {
+      this.id_visiteur = json.id_visiteur;
+      this.nom_visiteur = json.nom_visiteur;
+      this.prenom_visiteur = json.prenom_visiteur;
+      this.type_visiteur = json.type_visiteur;
+      this.login = json.login;
+      this.password = json.password;
+    } else {
+      this.id_visiteur = 0;
+      this.nom_visiteur = '';
+      this.prenom_visiteur = '';
+      this.type_visiteur = '';
+      this.login = '';
+      this.password = '';
+    }
+  }
+}
