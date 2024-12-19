c18abc1cea126f6d2c42e0768499cdfdc3d6ba5e
Reark-smey <rearksmey.by@Lmdsio.fr>
2024-12-19 17:42:00 +0100

    gsb

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import {Routes, Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {GsbLoginService} from '../gsb-login.service';
+
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private loginService: GsbLoginService) {
+    this.loginForm = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid){
+      const{login, password } = this.loginForm.value;
+      this.loginService.serviceEnvoieLogin(login,password).subscribe( {
+        next: data => console.log('Login successful', data),
+        error: error => console.error('Login failed', error)
+      });
+    }
+  }
 }
