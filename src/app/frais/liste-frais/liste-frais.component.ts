019ed3e4c6b6ee6701a1b304040b38c76986808a
Reark-smey <rearksmey.by@Lmdsio.fr>
2024-12-19 16:47:49 +0100

    gsb

---
+++ src/app/frais/liste-frais/liste-frais.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-frais',
+  imports: [],
+  templateUrl: './liste-frais.component.html',
+  standalone: true,
+  styleUrl: './liste-frais.component.css'
+})
+export class ListeFraisComponent {
+
+}
